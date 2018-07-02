d67f8c3e57ccc2a881abb6a7795ab4a311a4910e
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-07-02 17:10:30 -0600

    app-5 first commit

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Image myImage= {'https://bloximages.newyork1.vip.townnews.com/pilotonline.com/content/tncms/assets/v3/editorial/c/d5/cd5dde92-1b18-5c2a-a624-a9752297ed3f/57467adbdc1dc.image.jpg'} />
       </div>
     );
   }
