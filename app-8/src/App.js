be90edcd76c0ac3b5859a3dcf44acd5441e9596b
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-07-15 15:23:38 -0600

    app 8 commit

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      person: ''
+    }
+  }
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/1").then(res => {
+      this.setState({person: res.data})
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div className="App">
+      <h1>Name: {this.state.person.name}</h1>
+      <h1>Birth: {this.state.person.birth_year}</h1>
+      <h1>Height: {this.state.person.height}</h1>
+      <h1>Eye Color: {this.state.person.eye_color}</h1>
+        
+    </div>
     );
   }
 }
